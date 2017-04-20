48484765fbab6b49a8a9fcf529f2f2012be76503
hwalborn <hwalborn@gmail.com>
2017-04-20 14:47:43 -0400

    putting react up there

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Map from './components/google-map'
 import './App.css';
+import axios from 'axios'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: null,
+      long:null,
+      zoom: 16,
+      wifis: []
+    }
+  }
+
+  componentWillMount(){
+    axios.get('https://data.cityofnewyork.us/api/views/jd4g-ks2z/rows.json?accessType=DOWNLOAD')
+    .then((response)=> {
+      this.setState({wifis: response.data.data})
+    })
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.setState({
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      })
+    })
+  }
+
   render() {
+    let stuff
+    if(!!(this.state.lat && this.state.long)){
+      stuff = <Map lat={this.state.lat} long={this.state.long} zoom={this.state.zoom} wifis={this.state.wifis} />
+    }else{
+      stuff = <h1>LOADING BITCHES</h1>
+    }
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {stuff}
       </div>
+
     );
   }
 }
---
+++ src/components/wifi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Marker from 'google-map-react'
+
+class Wifi extends React.Component {
+  render(){
+    return (
+      <p>HOLT</p>
+    )
+  }
+}
+
+export default Wifi
